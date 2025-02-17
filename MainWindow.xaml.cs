3a8f7da63639708dc1f1f29966e223bff430433f
Dániel Bordás <bordas.daniel0124@gmail.com>
2025-02-17 08:56:04 +0100

    GUI jobb oldala kidolgozva

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO;
+using Microsoft.Win32;
 
 namespace IrodalomProjektOrai
 {
@@ -24,7 +25,14 @@ namespace IrodalomProjektOrai
 
         private static void LoadFromFiles(string FileNameWPath)
         {
-
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Szöveges állomány (*.txt)|*.txt|Táblázat (*.csv)|*.csv";
+            openFileDialog.Title = "Fájl megnyitása";
+            bool? gotValue = openFileDialog.ShowDialog();
+            if (gotValue == true)
+            {
+                
+            }
         }
 
         private void BtnLoad_Click(object sender, RoutedEventArgs e)
@@ -41,5 +49,22 @@ namespace IrodalomProjektOrai
         {
             MessageBox.Show("");
         }
+        private void BtnPrevious_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("");
+        }
+
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("");
+        }
+
+        private void BtnNext_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("");
+        }
+
+
     }
 }
